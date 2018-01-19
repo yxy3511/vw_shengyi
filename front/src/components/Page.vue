cc79aec247dcca9db91cf42cfeb07e9c1a216d10
yuxinyan <yuxinyan@addnewer.com>
2018-01-19 14:39:48 +0800

    first

---
+++ front/src/components/Page.vue
@@ -0,0 +1,33 @@
+<template lang="jade">
+  h1 伍嘉成
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='sass'>
+  h1, h2
+    font-weight: normal
+
+  ul
+    list-style-type: none
+    padding: 0
+
+  li
+    display: inline-block
+    margin: 0 10px
+
+  a
+    color: #42b983
+
+</style>
